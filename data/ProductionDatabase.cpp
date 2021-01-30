030d686bf6db2c2546b8e0ad33e76b09541fd165
Paolo Valerdi <paolovalerdi@gmail.com>
2021-01-30 17:56:58 -0600

    Use ODBC driver

--- data/ProductionDatabase.cpp
+++ data/ProductionDatabase.cpp
@@ -12,7 +12,7 @@ ProductionDatabase* ProductionDatabase::getInstance()
 
 ProductionDatabase::ProductionDatabase()
 {
-    database = QSqlDatabase::addDatabase("QMYSQL");
+    database = QSqlDatabase::addDatabase("QODBC");
     database.setHostName("localhost");
     database.setDatabaseName("fonditapp");
     database.setUserName("root");
