b57d0c51dc3b111b1dbc7e5c9ec88d236e9d5ccf
Paolo Valerdi <paolovalerdi@gmail.com>
2021-02-10 21:23:36 -0600

    waiter: Fix build

--- waiter-app-qml/main.cpp
+++ waiter-app-qml/main.cpp
@@ -2,29 +2,17 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
 #include "OrderListModel.h"
-#include "OrderListModelCallback.h"
 
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-
     QGuiApplication app(argc, argv);
-
     QQmlApplicationEngine engine;
 
-    // Registramos las clases de C++ que sirven como modelos para QML
     qmlRegisterType<OrderListModel>("Order", 1, 0, "OrderListModel");
-    qmlRegisterUncreatableType<OrderListModelCallback>("Order", 1, 0, "OrderListModelCallback", "Can't create instaces of this class");
-
-    OrderListModelCallback orderListModelCallback;
-    engine.rootContext()->setContextProperty("orderListModelCallback", &orderListModelCallback);
-
-
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
@@ -33,6 +21,5 @@ int main(int argc, char *argv[])
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
-
     return app.exec();
 }
