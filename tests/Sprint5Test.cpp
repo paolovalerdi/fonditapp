2d423f22f13f76e6a5cb111ebc7b97c3917e989e
Mariana <mariana.floresb29@gmail.com>
2021-02-16 14:06:11 -0600

    Test Get resquested bills

--- tests/Sprint5Test.cpp
+++ tests/Sprint5Test.cpp
@@ -8,4 +8,26 @@ TEST_CASE("Update order status") {
     orderDao.updateOrderStatus(testOrder);
     testOrder = orderDao.getOrderById(1);
     REQUIRE(testOrder.getId_status() == 4);
+
 }
+
+TEST_CASE("Request bill")
+{
+    OrderDao orderDao=OrderDao(new TestDatabase());
+    auto numResquest=orderDao.getRequest();
+    REQUIRE(numResquest==1);
+}
+
+TEST_CASE("Update request")
+{
+    OrderDao orderDao=OrderDao(new TestDatabase());
+    auto testOrder2 = orderDao.getOrderById(1);
+    qDebug()<<QString::number(testOrder2.getCall_waiter()); //0
+    orderDao.updateRequest(testOrder2.getId_order());
+    testOrder2 = orderDao.getOrderById(1);
+    qDebug()<<QString::number(testOrder2.getCall_waiter());
+
+    REQUIRE(testOrder2.getCall_waiter()==1);
+}
+
+
