33667f50784e224fc41b2429b6389af38bc9fefe
Paolo Valerdi <paolovalerdi@gmail.com>
2021-02-17 16:09:25 -0600

    customer: Reformatting using custom styling

--- customer-app-qml/CategoryListModel.h
+++ customer-app-qml/CategoryListModel.h
@@ -9,23 +9,23 @@
 
 class CategoryListModel : public QAbstractListModel
 {
-  Q_OBJECT
+		Q_OBJECT
 
-public:
-  enum Roles {
-    ID_ROLE,
-    TITLE_ROLE,
-    ICON_ROLE
-  };
+	public:
+		enum Roles {
+			ID_ROLE,
+			TITLE_ROLE,
+			ICON_ROLE
+		};
 
-  explicit CategoryListModel(QObject *parent = nullptr);
-  int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-  QHash<int, QByteArray> roleNames() const override;
+		explicit CategoryListModel(QObject *parent = nullptr);
+		int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+		QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+		QHash<int, QByteArray> roleNames() const override;
 
-private:
-  QList<Category> categories;
-  ProductsDao productDao = ProductsDao(ProductionDatabase::getInstance());
+	private:
+		QList<Category> categories;
+		ProductsDao productDao = ProductsDao(ProductionDatabase::getInstance());
 
-  QString getImagePath(int categoryId) const;
+		QString getImagePath(int categoryId) const;
 };
